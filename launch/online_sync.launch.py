6c8c53abe942441254dfa3e7b18ee07714326261
Haruki Isono <haruki.isono861@gmail.com>
2026-06-21 15:00:58 +0900

    chore:

--- launch/online_sync.launch.py
+++ launch/online_sync.launch.py
@@ -1,66 +1,96 @@
 import os
 
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, LogInfo
-from launch.conditions import UnlessCondition
-from launch.substitutions import LaunchConfiguration, PythonExpression
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from nav2_common.launch import HasNodeParams
+from launch import LaunchDescription
+from launch.actions import (DeclareLaunchArgument, EmitEvent, LogInfo,
+                            RegisterEventHandler)
+from launch.conditions import IfCondition
+from launch.events import matches_action
+from launch.substitutions import (AndSubstitution, LaunchConfiguration,
+                                  NotSubstitution)
+from launch_ros.actions import LifecycleNode
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
+from lifecycle_msgs.msg import Transition
+from launch_ros.descriptions import ParameterFile
 
 
 def generate_launch_description():
+    autostart = LaunchConfiguration('autostart')
+    use_lifecycle_manager = LaunchConfiguration("use_lifecycle_manager")
     use_sim_time = LaunchConfiguration('use_sim_time')
-    params_file = LaunchConfiguration('params_file')
-    default_params_file = os.path.join(get_package_share_directory("lucia_slam_toolbox"), 'config', 'mapper_params_online_async.yaml')
-    rviz_config_dir = os.path.join(get_package_share_directory('lucia_slam_toolbox'), 'rviz', 'slam_toolbox.rviz')
+    slam_params_file = LaunchConfiguration('slam_params_file')
 
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart', default_value='true',
+        description='Automatically startup the slamtoolbox. '
+                    'Ignored when use_lifecycle_manager is true.')
+    declare_use_lifecycle_manager = DeclareLaunchArgument(
+        'use_lifecycle_manager', default_value='false',
+        description='Enable bond connection during node activation')
     declare_use_sim_time_argument = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='false',
-        description='Use simulation (Gazebo) clock if true'
-    )
-    declare_params_file_cmd = DeclareLaunchArgument(
-        'params_file',
-        default_value=default_params_file,
+        default_value='true',
+        description='Use simulation/Gazebo clock')
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=os.path.join(get_package_share_directory("slam_toolbox"),
+                                   'config', 'mapper_params_online_sync.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
-    has_node_params = HasNodeParams(source_file=params_file,
-                                      node_name='slam_toolbox',)
-
-    actual_params_file = PythonExpression([
-        '"', params_file, '" if ', has_node_params, ' else "', default_params_file, '"'
-    ])
-
-    log_params_file = LogInfo(msg=['provid params_file:', params_file,
-                                   'does not contain slam_toolbox paramerters. Using default:',
-                                   default_params_file],
-                              condition=UnlessCondition(has_node_params))
-
-    start_sync_slam_toolbox_node = Node(
+    # Perform substitution `$find-pkg-share`
+    slam_params_file_w_subst = ParameterFile(
+        slam_params_file,
+        allow_substs=True,
+    )
+    
+    start_sync_slam_toolbox_node = LifecycleNode(
         parameters=[
-            actual_params_file,
-            {'use_sim_time': use_sim_time}
+          slam_params_file_w_subst,
+          {
+            'use_lifecycle_manager': use_lifecycle_manager,
+            'use_sim_time': use_sim_time
+          }
         ],
         package='slam_toolbox',
         executable='sync_slam_toolbox_node',
         name='slam_toolbox',
-        output='screen')
+        output='screen',
+        namespace=''
+    )
 
-    rviz2 = Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            parameters=[{'use_sim_time': use_sim_time}],
-            output='screen')
+    configure_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(start_sync_slam_toolbox_node),
+            transition_id=Transition.TRANSITION_CONFIGURE
+        ),
+        condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
+    )
+
+    activate_event = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=start_sync_slam_toolbox_node,
+            start_state="configuring",
+            goal_state="inactive",
+            entities=[
+                LogInfo(msg="[LifecycleLaunch] Slamtoolbox node is activating."),
+                EmitEvent(event=ChangeState(
+                    lifecycle_node_matcher=matches_action(start_sync_slam_toolbox_node),
+                    transition_id=Transition.TRANSITION_ACTIVATE
+                ))
+            ]
+        ),
+        condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
+    )
 
     ld = LaunchDescription()
 
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_lifecycle_manager)
     ld.add_action(declare_use_sim_time_argument)
-    ld.add_action(declare_params_file_cmd)
-    ld.add_action(log_params_file)
+    ld.add_action(declare_slam_params_file_cmd)
     ld.add_action(start_sync_slam_toolbox_node)
-    ld.add_action(rviz2)
+    ld.add_action(configure_event)
+    ld.add_action(activate_event)
 
     return ld
